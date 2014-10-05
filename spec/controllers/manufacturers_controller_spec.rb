a9a4e22212a04fc837a75226c8a45339d4eb7909
Frank Zhu <frankyufengzhu@gmail.com>
2014-10-05 16:56:32 -0400

    Added tests for manufacturer

--- spec/controllers/manufacturers_controller_spec.rb
+++
@@ -1,5 +0,0 @@
-require 'rails_helper'
-
-RSpec.describe carsController, :type => :controller do
-
-end
---
+++ spec/features/manufacturer_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+feature 'New manufacturer' do
+  scenario 'User tries to add a new manufacturer' do
+    visit manufacturers_path
+    click_on 'Add A New Manufacturer'
+    fill_in 'Name', with: 'ZoomZoom'
+    fill_in 'Country', with: 'deDust'
+    click_button 'Create Manufacturer'
+
+    expect(page).to have_content 'Manufacturer submitted'
+  end
+
+  scenario 'User tries to add a manufacturer already exists' do
+    let(:manufacturer) { FactoryGirl.create(:manufacturer) }
+    visit manufacturers_path
+    click_on 'Add A New Manufacturer'
+    fill_in 'Name', with: 'Honda'
+    fill_in 'Country', with: 'deDust'
+    click_button 'Create Manufacturer'
+
+    expect(page).to have_content 'Invalid entry'
+  end
+end
